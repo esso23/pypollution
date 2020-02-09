01a81ef79d867d6369eb1a547ff7b61e72b401bd
Esso <akmetik@gmail.com>
2020-02-09 20:18:31 +0100

    Map presets.

--- prototypes/map-preset.lua
+++ prototypes/map-preset.lua
@@ -4,8 +4,47 @@ data.raw["map-gen-presets"]["default"]["pyanodon-default"] =
 	{
       enemy_evolution =
 	  {
-        pollution_factor = 0.3e-06,
-        time_factor = 0.5e-06
+        pollution_factor = 3e-07,
+        time_factor = 5e-07,
+		destroy_factor = 75e-05
+      },
+      pollution =
+	  {
+        ageing = 1.0,
+        enemy_attack_pollution_consumption_modifier = 1.0
+      }
+    },
+    order = "p"
+}
+
+data.raw["map-gen-presets"]["default"]["pyanodon-easy"] = 
+{
+    advanced_settings =
+	{
+      enemy_evolution =
+	  {
+        pollution_factor = 2e-07,
+        time_factor = 0,
+		destroy_factor = 50e-05
+      },
+      pollution =
+	  {
+        ageing = 1.0,
+        enemy_attack_pollution_consumption_modifier = 1.0
+      }
+    },
+    order = "p"
+}
+
+data.raw["map-gen-presets"]["default"]["pyanodon-hard"] = 
+{
+    advanced_settings =
+	{
+      enemy_evolution =
+	  {
+        pollution_factor = 4e-07,
+        time_factor = 15e-07,
+		destroy_factor = 100e-05
       },
       pollution =
 	  {
