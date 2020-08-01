5e5316ff09e1f3e4e0f3da5908b12de47620d1db
Esso <akmetik@gmail.com>
2020-08-01 13:30:02 +0200

    Fix issue in SetRecipeResultAmount
    - https://mods.factorio.com/mod/pypollution/discussion/5f247f165469b6ced8dd3c2e

--- prototypes/common.lua
+++ prototypes/common.lua
@@ -1,9 +1,16 @@
 function SetRecipeResultAmount(recipe, result, newAmount)
-  local results = data.raw.recipe[recipe].results
-  for _,v in pairs(results) do
-    if (v.name == result) then
-	  v.amount = newAmount
-	  --log("Setting amount to " .. newAmount .. " on recipe's " .. recipe .. " result " .. result)
+	local recipe = data.raw.recipe[recipe]
+	
+	if recipe == nil then
+		log(recipe .. " not found.")
+		return
 	end
-  end
+
+	local results = recipe.results
+	for _,v in pairs(results) do
+		if (v.name == result) then
+			v.amount = newAmount
+			--log("Setting amount to " .. newAmount .. " on recipe's " .. recipe .. " result " .. result)
+		end
+	end
 end
